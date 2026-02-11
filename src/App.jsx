a89ebc33f0410058729cbc48fb99d8f826b44f2c
vandhanad779-glitch <21951a67h0@iare.ac.in>
2026-02-12 01:15:36 +0530

    updated data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import BrowseBooks from "./pages/BrowseBooks";
-import BookDetails from "./Pages/BookDetails";
+import BookDetails from "./pages/BookDetails";
 import AddBook from "./pages/AddBook";
 import NotFound from "./pages/NotFound";
 
-function App(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                {/* Routes With Navbar */}
-                <Route path="/" element ={<><Navbar/><Home/></>}/>
-                <Route path="/books" element={<><Navbar/><BrowseBooks/></>}/>
-                <Route path="/books/:category" element={<><Navbar /><BrowseBooks /></>} />
-                <Route path="/book/:id" element={<><Navbar /><BookDetails /></>} />
-                <Route path="/add-book" element={<><Navbar /><AddBook /></>} />
-                {/* 404 Without Navbar */}
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<><Navbar /><Home /></>} />
+      <Route path="/books" element={<><Navbar /><BrowseBooks /></>} />
+      <Route path="/books/:category" element={<><Navbar /><BrowseBooks /></>} />
+      <Route path="/book/:id" element={<><Navbar /><BookDetails /></>} />
+      <Route path="/add-book" element={<><Navbar /><AddBook /></>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import {store} from "./redux/store";
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
